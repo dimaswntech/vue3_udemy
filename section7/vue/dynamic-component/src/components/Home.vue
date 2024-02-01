2535d1fb33472aa897fb81bbeb61646cb522f167
dimaswntech <dimaswntech@gmail.com>
2024-02-01 13:07:39 +0700

    section7

---
+++ section7/vue/dynamic-component/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Home Page</h1>
+</template>
+<script >
+export default {
+  name:'Home',
+  unmounted() {
+    console.log('Home component unmounted')
+  },
+  activated() {
+    console.log('Home activeted')
+  },
+  deactivated() {
+    console.log('Home deactiveted')
+  }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
